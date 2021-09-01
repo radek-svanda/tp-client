c8409827001b36e5eaed074870e2ffd1ba847fb1
Radek Svanda <radek.svanda@elder.cz>
2021-09-01 23:39:02 +0200

    Initial version

---
+++ app/src/main/kotlin/tp/client/time/TimeCommand.kt
@@ -0,0 +1,8 @@
+package tp.client.time
+
+import com.github.ajalt.clikt.core.CliktCommand
+
+class TimeCommand : CliktCommand() {
+    override fun run() {
+    }
+}
---
+++ app/src/main/kotlin/tp/client/time/show/ShowCommand.kt
@@ -0,0 +1,9 @@
+package tp.client.time.show
+
+import com.github.ajalt.clikt.core.CliktCommand
+
+class ShowCommand : CliktCommand() {
+    override fun run() {
+        TODO("Show timesheet for current month")
+    }
+}
